2c6967396e7801c39a175719d4d7264b3311f873
Hira-shi <carreljeremie@gmail.com>
2025-02-15 16:03:06 +0100

    win condition

--- game.c
+++ game.c
@@ -1,3 +1,38 @@
-#include <game.h>
+#include "game.h"
+#include <stdio.h>
 
-void print_board(board_t game);
+#include <stdbool.h>
+
+void print_board(board_t game){
+    printf("%c | %c | %c \n ----------- \n %c | %c | %c \n ----------- \n %c | %c | %c", game.cases[0], game.cases[1], game.cases[2], game.cases[3], game.cases[4], game.cases[5], game.cases[6], game.cases[7], game.cases[8],);
+}
+
+bool check_win(board_t game){
+    if(game.cases[0] == game.cases[1] == game.cases[2] || game.cases[3] == game.cases[4] == game.cases[5] || game.cases[6] == game.cases[7] == game.cases[8]){
+        return true;
+    }else if(game.cases[0] == game.cases[3] == game.cases[6] || game.cases[1] == game.cases[4] == game.cases[7] || game.cases[2] == game.cases[5] == game.cases[8]){
+        return true;
+    }else if(game.cases[0] == game.cases[4] == game.cases[8] || game.cases[6] == game.cases[4] == game.cases[2]){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool is_board_full(board_t game){
+    for(int i = 0; i <= 9; i++){
+        if(game.cases[i] == ' '){
+            return false;
+        }
+    }
+    return true;
+
+}
+
+bool is_game_over(board_t game){
+    if(is_board_full(game) || check_win(game)){
+        return true;
+    }else{
+        return false;
+    }
+}
--- game.h
+++ game.h
@@ -1,18 +1,16 @@
 #ifndef GAME_H
 #define GAME_H
 
+#pragma once
+
+
+
+
 typedef struct {
-    char case1;
-    char case2;
-    char case3;
-    char case4;
-    char case5;
-    char case6;
-    char case7;
-    char case8;
-    char case9;
+    char cases[9]
 } board_t;
 
 void print_board(board_t game);
+
 
 #endif // Game TicTacToe
